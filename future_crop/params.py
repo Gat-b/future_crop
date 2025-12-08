4e8117a795f52862896e060321027d62a30530ef
gregoire caye <greg.caye@gmail.com>
2025-12-08 16:53:06 +0100

    data-viz updated, empty files for workflow, upadted function file with saving and loading mechanisms

---
+++ future_crop/params.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ################## TBU
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_PROJECT_WAGON = os.environ.get("GCP_PROJECT_WAGON")
+GCP_REGION = os.environ.get("GCP_REGION")
+BQ_DATASET = os.environ.get("BQ_DATASET")
+BQ_REGION = os.environ.get("BQ_REGION")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+INSTANCE = os.environ.get("INSTANCE")
+MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
+MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
+MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
+PREFECT_FLOW_NAME = os.environ.get("PREFECT_FLOW_NAME")
+PREFECT_LOG_LEVEL = os.environ.get("PREFECT_LOG_LEVEL")
+
+PLATFORM = os.environ.get("PLATFORM")
+
+##################  CONSTANTS  #####################
+if PLATFORM == "local":
+    LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), "gat_b", "future_crop", "raw_data")
+    LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), "gat_b", "future_crop", "processed_data")
+    MODEL_PATH_STORAGE = os.path.join(os.path.expanduser('~'), "gat_b", "future_crop", "models")
+    RESULT_PATH_STORAGE = os.path.join(os.path.expanduser('~'), "gat_b", "future_crop", "yield_forecasts")
+
+elif PLATFORM == "gcp":
+    pass
+
+COLUMN_NAMES_RAW = ['fare_amount','pickup_datetime', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'passenger_count']
+
+DTYPES_RAW = {
+}
+
+DTYPES_PROCESSED = np.float32
+
+
+################## VALIDATIONS #################
